21bcb630ebc016118dac4ddca0895660b98fda4c
Ubuntu <ubuntu@ip-172-31-42-188.ec2.internal>
2019-05-29 20:34:50 +0000

    test1

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 require_relative 'boot'
 
+
+
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -10,7 +12,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -30,7 +32,7 @@ module EtchASketchBE
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = false
+    config.api_only = true
 
     #config.action_dispatch.default_headers = {
     #  'Access-Control-Allow-Origin' => '*',
@@ -44,7 +46,11 @@ module EtchASketchBE
 #   end
 #end
 
-
+# Middleware for ActiveAdmin
+config.middleware.use Rack::MethodOverride
+config.middleware.use ActionDispatch::Flash
+config.middleware.use ActionDispatch::Cookies
+config.middleware.use ActionDispatch::Session::CookieStore
 
 
 
