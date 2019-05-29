22525e3cd6cc0534fba41216d5eae52f419a82dc
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-29 11:38:56 -0500

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+Sketch.destroy_all
+
+user1 = User.create(username: "JSmith")
+user2 = User.create(username: "JDoe")
+
+img = "0"*(412*277);
+
+Sketch.create( user_id:user1.id, pointerX:100, pointerY:100, width:412, height:277, data: img)
+Sketch.create(user_id: user1.id, pointerX:150, pointerY:150, width:412, height:277, data:img)
+Sketch.create( user_id:user2.id, pointerX:100, pointerY: 100, width: 412, height: 277, data: img)
+Sketch.create( user_id:user2.id, pointerX:150, pointerY:150, width:412, height:277, data:img)
+
